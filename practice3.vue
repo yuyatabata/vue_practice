a43ff015decf7e2b6729376253028d9cc6abef29
田端 佑也 <s09940@ca-10012317.cyberagent.co.jp>
2020-03-24 09:58:16 +0900

    add3

---
+++ practice3.vue
@@ -0,0 +1,14 @@
+Vue.component('fruits-list', {
+	props:['fruitsItem'],
+  template: '<li>{{fruitsItem.name}}</li>'
+});
+
+new Vue({
+	el: '#fruits-component',
+  data: {
+  	fruitsItems: [
+    	{name: '梨'},
+      {name: 'イチゴ'}
+    ]
+  }
+});
